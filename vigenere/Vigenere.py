770ca5eadd2b4ca005b40bc817e177e4037305d1
= <branchjohnchristopher@gmail.com>
2024-03-28 20:24:24 -0500

    added CLI logic and added alternative encrypt function

--- vigenere/Vigenere.py
+++ vigenere/Vigenere.py
@@ -1,5 +1,27 @@
-plain_text = input("Input the plain_text: ")
-key = input("Input the key: ")
+
+def encrypt_v0_2(src: str, key: str) -> str:
+    """
+    Using the specified key, encrypt source text using vigenere cipher.
+
+    The if the key contains non-alpha characters, it will fail (for now)
+    """
+    result = ""
+    for i, c in enumerate(src):
+        if c.isalpha():
+            # offset is used so we can treat values as being
+            #  characters represented 0-25 rather than raw ascii.
+            #  It also makes it easier to treat capital and lowercase
+            #  the same.
+            m_offset = 65 if c.isupper() else 97
+            k = ord(key[i % len(key)])
+            k_offset = 65 if chr(k).isupper() else 97
+            k -= k_offset
+            nc = (ord(c)-m_offset + k) % 26 + m_offset # new character
+            result += chr(nc)
+        else:
+            # for now, do not alter numbers and symbols
+            result += c
+    return result
 
 def encrypt(plain_text, key):
     # The cipher variable will store the final encrypted key
@@ -67,6 +89,25 @@ def encrypt(plain_text, key):
     # At the end return the finished cipher
     print(cipher)
 
-encrypt(plain_text, key)
+if __name__ == "__main__":
+    # import these only when using CLI tool
+    from sys import stdin
+    from argparse import ArgumentParser
 
-# New Comment
+    # parse CLI arguments
+    parser = ArgumentParser(
+        prog='Vigenere Cipher Tool',
+        description='Can be used to encrypt or decrypt using the vigenere cipher',
+        epilog='Written by Kyle Stewart, Katie Sparr, and Jack Branch'
+    )
+    mode_group = parser.add_mutually_exclusive_group(required = True)
+    mode_group.add_argument('-e', '--encrypt', help='Encrypts STDIN with provided key')
+    mode_group.add_argument('-d', '--decrypt', help='Decrypts STDIN with provided key')
+    args = parser.parse_args()
+
+    if args.encrypt is not None:
+        print(encrypt_v0_2(stdin.read(), args.encrypt))
+    elif args.decrypt is not None:
+        print("decrypt functionality not yet implemented")
+
+

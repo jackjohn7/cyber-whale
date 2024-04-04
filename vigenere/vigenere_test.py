da71f2f613ff517df149e3bcb7871198233b4e53
= <branchjohnchristopher@gmail.com>
2024-04-03 21:48:14 -0500

    added tests

--- vigenere/vigenere_test.py
+++ vigenere/vigenere_test.py
@@ -5,3 +5,9 @@ def test_encrypt():
     assert vigenere.encrypt_v0_2("sponge", "hello") == "ztzyul"
     assert vigenere.encrypt_v0_2("message longer than key", "small") == "eqsdlyq lzyyqr essz kpj"
     assert vigenere.encrypt_v0_2("small", "keylongerthanmsg") == "cqywz"
+
+def test_decrypt():
+    assert vigenere.decrypt("tthex", "test") == "apple"
+    assert vigenere.decrypt("ztzyul", "hello") == "sponge"
+    assert vigenere.decrypt("eqsdlyq lzyyqr essz kpj", "small") == "message longer than key"
+    assert vigenere.decrypt("cqywz", "keylongerthanmsg") == "small"

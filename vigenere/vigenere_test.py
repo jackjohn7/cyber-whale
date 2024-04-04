c43a4ddc32a1cb579c8db1fef10795e45ebbcefe
= <branchjohnchristopher@gmail.com>
2024-04-03 21:51:16 -0500

    renamed encrypt and unqualified imports in test

--- vigenere/vigenere_test.py
+++ vigenere/vigenere_test.py
@@ -1,13 +1,13 @@
-import vigenere
+from vigenere import encrypt, decrypt
 
 def test_encrypt():
-    assert vigenere.encrypt_v0_2("apple", "test") == "tthex"
-    assert vigenere.encrypt_v0_2("sponge", "hello") == "ztzyul"
-    assert vigenere.encrypt_v0_2("message longer than key", "small") == "eqsdlyq lzyyqr essz kpj"
-    assert vigenere.encrypt_v0_2("small", "keylongerthanmsg") == "cqywz"
+    assert encrypt("apple", "test") == "tthex"
+    assert encrypt("sponge", "hello") == "ztzyul"
+    assert encrypt("message longer than key", "small") == "eqsdlyq lzyyqr essz kpj"
+    assert encrypt("small", "keylongerthanmsg") == "cqywz"
 
 def test_decrypt():
-    assert vigenere.decrypt("tthex", "test") == "apple"
-    assert vigenere.decrypt("ztzyul", "hello") == "sponge"
-    assert vigenere.decrypt("eqsdlyq lzyyqr essz kpj", "small") == "message longer than key"
-    assert vigenere.decrypt("cqywz", "keylongerthanmsg") == "small"
+    assert decrypt("tthex", "test") == "apple"
+    assert decrypt("ztzyul", "hello") == "sponge"
+    assert decrypt("eqsdlyq lzyyqr essz kpj", "small") == "message longer than key"
+    assert decrypt("cqywz", "keylongerthanmsg") == "small"

c2464eb7d85b4e48a6ad7d07e899e979697bc757
= <branchjohnchristopher@gmail.com>
2024-04-10 23:24:51 -0500

    got my original function working

--- chat_timing/reader.py
+++ chat_timing/reader.py
@@ -1,7 +1,7 @@
 
 def to_cov_bin(data: list[float], zero: float, one: float) -> str:
 
-    diffs: list[float] = [0.0] * len(data)
+    diffs: list[float] = [0.0] * (len(data)-1)
     i, j = 0, 1
     while j < len(data):
         diffs[i] = round(data[j] - data[i], 3)
@@ -40,7 +40,6 @@ if __name__ == "__main__":
     import socket
     import sys
     import time
-    from binascii import unhexlify
     ip = "localhost"
     #ip = "138.47.165.156"
     port = 1337
@@ -59,15 +58,19 @@ if __name__ == "__main__":
     s.close()
 
     mapped_binary = to_cov_bin(chat_delays, 0.025, 0.1)
-    hex_string = hex(int(mapped_binary, 2))[2:]
-    if len(hex_string) % 2 != 0:
-        hex_string = '0' + hex_string
+    result = "".join([chr(int(mapped_binary[i:i+8], 2)) for i in range(0, len(mapped_binary), 8)])[0:-3]
+    print("\n", result)
 
-    print(hex_string)
-    decoded_bytes = unhexlify(hex_string)
-    print(decoded_bytes)
+    # Also works
+    #hex_string = hex(int(mapped_binary, 2))[2:]
+    #if len(hex_string) % 2 != 0:
+    #    hex_string = '0' + hex_string
 
-    decoded_message = decoded_bytes.decode('utf-8')
+    #print(hex_string)
+    #decoded_bytes = unhexlify(hex_string)
+    #print(decoded_bytes)
 
-    print(decoded_message[:-3])
+    #decoded_message = decoded_bytes.decode('utf-8')
+
+    #print(decoded_message[:-3])
 

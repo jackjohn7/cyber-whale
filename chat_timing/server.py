d22cafa1970156b64c5e28ac423a124903afa596
jackjohn7 <70782491+jackjohn7@users.noreply.github.com>
2024-04-26 22:53:19 -0500

    cleaned up, improved server, added explanation for rust implementation

--- chat_timing/server.py
+++ chat_timing/server.py
@@ -1,5 +1,15 @@
 import socket
 import time
+import sys
+import signal
+
+def new_int_handler(conn: socket.socket):
+    def interrupt_handler(_sig, _frame):
+        conn.close()
+        print("Connection closed, quitting...")
+        sys.exit(0)
+    return interrupt_handler
+
 
 EOF = "EOF"
 ZERO = 0.025
@@ -31,22 +41,30 @@ s.bind(("", port))
 
 s.listen(0)
 
+signal.signal(signal.SIGINT, new_int_handler(s))
+
 # respond to every connection synchronously
 while True:
-    # accept connections
-    c, addr = s.accept()
+    try:
+        # accept connections
+        c, addr = s.accept()
 
-    print(f"Incoming connection from {addr}")
-    msg = "When the "
-    secret_msg = "I am inside your walls" + "EOF"
-    secret_encoded = "".join([bin(ord(c))[2:].zfill(8) for c in secret_msg])
-    print(secret_encoded)
+        print(f"Incoming connection from {addr}")
+        msg = "When the "
+        secret_msg = "I am inside your walls" + "EOF"
+        secret_encoded = "".join([bin(ord(c))[2:].zfill(8) for c in secret_msg])
+        print(secret_encoded)
 
-    for i, ch in enumerate(secret_encoded):
-        # needs to be converted to individual bytes to work
-        c.send(msg[i%len(msg)].encode())
-        time.sleep(ZERO if ch == "0" else ONE)
+        for i, ch in enumerate(secret_encoded):
+            # needs to be converted to individual bytes to work
+            c.send(msg[i%len(msg)].encode())
+            time.sleep(ZERO if ch == "0" else ONE)
 
-    c.send("EOF".encode())
+        c.send("EOF".encode())
 
-    c.close()
+        c.close()
+    except KeyboardInterrupt:
+        s.close()
+        print("Connection closed, quitting...")
+        sys.exit(0)
+
--- chat_timing/src/main.rs
+++ chat_timing/src/main.rs
@@ -2,12 +2,13 @@ use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::time;
 
+// DELAYS (ms)
 const ONE: u128 = 100;
 const ZERO: u128 = 25;
 
 fn main() {
-    let ip = "127.0.0.1:1337";
-    // let ip = "138.47.165.156:31337"; // timo's server
+    //let ip = "127.0.0.1:1337"; // my local server
+    let ip = "138.47.165.156:31337"; // timo's server
     let mut timings = Vec::new();
     if let Ok(mut stream) = TcpStream::connect(ip) {
         let mut stdout = std::io::stdout().lock();
@@ -17,6 +18,7 @@ fn main() {
                 Ok(0) => break,
                 Ok(bytes) => {
                     timings.push(time::Instant::now());
+                    // print incoming unless it's EOF
                     if data[0..bytes] != [b'E', b'O', b'F'] {
                         stdout.write(&data[0..bytes]).unwrap();
                         stdout.flush().unwrap();
@@ -32,15 +34,16 @@ fn main() {
         panic!("failed to connect to server");
     }
 
-    let one = ONE - 5;
-    let zero = ZERO - 5;
+    // split on the average between ONE and ZERO
+    // This tended to produce more consistent results in our testing
+    let split = (ONE + ZERO) / 2;
 
     let mut hidden_msg = timings
         .windows(2)
         .map(|x| x[1].duration_since(x[0]).as_millis())
         .collect::<Vec<u128>>()
         .iter()
-        .map(mapper(one, zero))
+        .map(mapper(ONE, ZERO, split))
         .collect::<String>()
         .as_bytes()
         .chunks(8)
@@ -55,39 +58,19 @@ fn main() {
     }
 
     println!("{}", hidden_msg);
-
-    // The logic separated into pieces
-    //  This is just useful for debugging
-
-    //let diffs = timings
-    //    .windows(2)
-    //    .map(|x| (x[0], x[1]))
-    //    .map(|(a, b)| b.duration_since(a))
-    //    .map(|d| d.as_millis())
-    //    .collect::<Vec<u128>>();
-    //let binary = diffs.iter().map(mapper(one, zero)).collect::<String>();
-    //let r = binary
-    //    .as_bytes()
-    //    .chunks(8)
-    //    .map(|x| std::str::from_utf8(x).unwrap())
-    //    .map(|x| format!("{:0>8}", x))
-    //    .map(|x| u8::from_str_radix(&x, 2).unwrap())
-    //    .map(|x| x as char)
-    //    .collect::<String>();
-    //dbg!(r);
 }
 
 /// Creates mapping closure given a one and zero value
-fn mapper(one: u128, zero: u128) -> impl for<'a> Fn(&'a u128) -> char {
+fn mapper(one: u128, zero: u128, split: u128) -> impl for<'a> Fn(&'a u128) -> char {
     return move |x: &u128| {
-        if one > 0 {
-            if x.clone() > one as u128 {
+        if one > zero {
+            if x.clone() > split {
                 '1'
             } else {
                 '0'
             }
         } else {
-            if x.clone() > zero {
+            if x.clone() > split {
                 '0'
             } else {
                 '1'

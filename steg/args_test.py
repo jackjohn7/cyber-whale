8a78cda01625cfe329901836aa2d8e1ca3a3f546
jack branch <branchjohnchristopher@gmail.com>
2024-05-08 18:09:09 -0500

    fixed relative pathing issues in tests

--- steg/args_test.py
+++ steg/args_test.py
@@ -12,10 +12,10 @@ def test_file_not_found():
 def test_bad_param():
     with pytest.raises(cli_errors.InvalidParam):
         args = Args()
-        args.parse(["-s", "-b", "-oinvalid", "-i13", "-wsteg.py"])
+        args.parse(["-s", "-b", "-oinvalid", "-i13", "-wsteg/steg.py"])
     with pytest.raises(cli_errors.InvalidParam):
         args = Args()
-        args.parse(["-s", "-b", "-o10", "-iinvalid", "-wsteg.py"])
+        args.parse(["-s", "-b", "-o10", "-iinvalid", "-wsteg/steg.py"])
 
 def test_mutual_exclusion():
     with pytest.raises(cli_errors.TooManyModesSpecified):
@@ -38,21 +38,21 @@ def test_unrecognized():
 
 def test_parse():
     args = Args()
-    args.parse(["-s", "-b", "-o3", "-i5", "-wsteg.py"])
+    args.parse(["-s", "-b", "-o3", "-i5", "-wsteg/steg.py"])
     assert args.action == "store"
     assert args.data_mode == "bit"
     assert args.offset == 3
     assert args.interval == 5
-    assert args.wrapper == "steg.py"
+    assert args.wrapper == "steg/steg.py"
     assert args.hidden is None
 
     args = Args()
-    args.parse(["-s", "-b", "-o30", "-i52", "-w../README.md", "-hsteg.py"])
+    args.parse(["-s", "-b", "-o30", "-i52", "-wREADME.md", "-hsteg/steg.py"])
     print(args)
     assert args.action == "store"
     assert args.data_mode == "bit"
     assert args.offset == 30
     assert args.interval == 52
-    assert args.wrapper == "../README.md"
-    assert args.hidden == "steg.py"
+    assert args.wrapper == "README.md"
+    assert args.hidden == "steg/steg.py"
 

ad04d55801572b3e429b01b3a9c3ec130c13bdcb
krs063 <krs063@email.latech.edu>
2024-05-09 17:32:41 -0500

    fixed pytz calling error

--- timelock/timelock_test.py
+++ timelock/timelock_test.py
@@ -9,8 +9,8 @@ def test_valid_hash_string():
 
 # datetime(year, month, day[, hour[, minute[, second[, microsecond[,tzinfo]]]]])
 def test_time_elapsed():
-    assert get_time_elapsed(datetime(year=1999, month=12, day=31, hour=23, minute=59, second=59, tzinfo = pytz.UTC), 
-                            datetime(year=2013, month=5, day=6, hour=7, minute=43, second=25, tzinfo = pytz.UTC)) == 421141380
+    assert get_time_elapsed(datetime(year=1999, month=12, day=31, hour=23, minute=59, second=59, tzinfo = UTC), 
+                            datetime(year=2013, month=5, day=6, hour=7, minute=43, second=25, tzinfo = UTC)) == 421141380
     
 
     

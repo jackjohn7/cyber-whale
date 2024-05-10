5a3382d0e9823c54963a6fa293eb4a626edd5c27
jack branch <branchjohnchristopher@gmail.com>
2024-05-09 21:47:24 -0500

    implemented sentinel detection properly, added tests

--- steg/steg.py
+++ steg/steg.py
@@ -1,13 +1,23 @@
 from abc import ABC, abstractmethod
+import logging
 
-SENTINEL = [0x0, 0xFF, 0x0, 0x0, 0xFF, 0x0]
+SENTINEL = bytearray(b"\x00\xff\x00\x00\xff\x00")
+
+DEBUG = False
+logger = logging.getLogger(__name__)
+if not DEBUG:
+    logger.disabled = True
+else:
+    logging.basicConfig(filename='out.log', encoding='utf-8', level=logging.DEBUG)
 
 class Steg(ABC):
     @abstractmethod
-    def store(self) -> list[int]:
+    def store(self) -> bytearray:
+        """Store one file inside another file"""
         pass
     @abstractmethod
-    def retrieve(self) -> list[int]:
+    def retrieve(self) -> bytearray:
+        """Retrieve file from inside another file"""
         pass
 
 class StegByte(Steg):
@@ -18,28 +28,35 @@ class StegByte(Steg):
         self.interval = interval
 
     def store(self):
+        logger.debug("storing bytes")
         w = self.wrapper
         h = self.hidden
         offset = self.offset
+        # write hidden to wrapper
         for i in range(len(h)):
             w[offset] = h[i]
             offset += self.interval
 
+        # write sentinel to wrapper
         for i in range(len(SENTINEL)):
             w[offset] = SENTINEL[i]
             offset += self.interval
 
-        return list(w)
+        return w
 
     def retrieve(self):
+        logger.debug("retrieving bytes")
         w = self.wrapper
-        h = []
-        offset = self.offset
-        while offset < len(w):
+        h = bytearray()
+        # read data from hidden file until sentinel
+        for offset in range(self.offset, len(w), self.interval):
             b = w[offset]
+            if h[-len(SENTINEL):] == SENTINEL:
+                logger.debug("encountered sentinel")
+                h = h[:-len(SENTINEL)]
+                break
             h.append(b)
-            offset += self.interval
-        return list(h)
+        return h
 
 class StegBit(Steg):
     def __init__(self, wrapper, hidden, offset, interval):
@@ -49,63 +66,56 @@ class StegBit(Steg):
         self.interval = interval
 
     def store(self):
+        logger.debug("storing bits")
         w = self.wrapper
         h = self.hidden
         offset = self.offset
+        # write hidden to wrapper
         for i in range(len(h)):
             for _ in range(8):
                 w[offset] &= 0b11111110
                 w[offset] |= ((h[i] & 0b10000000) >> 7)
-                #h[i] <<= 1
                 h[i] = (h[i] << 1) & (2 ** 8 - 1)
                 offset += self.interval
 
+        # write sentinel to wrapper
         for i in range(len(SENTINEL)):
             for _ in range(8):
                 w[offset] &= 0b11111110
                 w[offset] |= ((SENTINEL[i] & 0b10000000) >> 7)
-                #SENTINEL[i] <<= 1
-                SENTINEL[i] = (SENTINEL[i] << 1) & (2 ** len(SENTINEL) - 1)
-        return list(w)
+                SENTINEL[i] = (SENTINEL[i] << 1) & (2 ** 8 - 1)
+                offset += self.interval
+        return w
 
     def retrieve(self):
+        logger.debug("retrieving bits")
         w = self.wrapper
         h = bytearray()
         offset = self.offset
-        s_count = -1
 
+        # read data from hidden file until sentinel or wrapper EOF
         while offset < len(w):
             b = 0
             for j in range(8):
-                b |= w[offset] & 0b1
+                b <<= 1
+                b |= w[offset] & 0b00000001
                 if j < 7:
-                    #b <<= 1
-                    b = (b << 1) & (2 ** 8 - 1)
                     offset += self.interval
-
-            if SENTINEL[s_count+1] == b:
-                print(s_count)
-                s_count += 1
-            else:
-                s_count = -1
-
-            if s_count == len(SENTINEL)-1:
-                h.append(b)
+            if h[-len(SENTINEL):] == SENTINEL:
+                logger.debug("encountered sentinel")
+                h = h[:-len(SENTINEL)]
                 break
-
-
             h.append(b)
             offset += self.interval
 
-        return list(h)[0:-len(SENTINEL)]
-
+        return h
 
 if __name__ == "__main__":
     from args import Args
-    from sys import argv
+    from sys import argv, stdout
     from traceback import format_exc
 
-    from sys import exit
+    from sys import exit, stderr
     import cli_errors
 
     args = Args()
@@ -132,20 +142,18 @@ if __name__ == "__main__":
             steg = StegBit(wrapper, hidden, args.offset, args.interval)
 
         if args.action == "retrieve":
-            print("".join([chr(c) for c in steg.retrieve()]))
+            stdout.buffer.write(steg.retrieve())
         else:
-            print("".join([chr(c) for c in steg.store()]))
+            stdout.buffer.write(steg.store())
+        logger.debug("wrote to file")
 
     except cli_errors.CliError as e:
-        print(e)
-        print(cli_errors.usage(argv[0]))
+        print(e, file=stderr)
+        print(cli_errors.usage(argv[0]), file=stderr)
         exit(1)
     except Exception as e:
-        print("Oops... An unexpected error occurred")
-        print(e)
-        print(format_exc())
-        print(cli_errors.usage(argv[0]))
+        print("Oops... An unexpected error occurred", file=stderr)
+        print(e, file=stderr)
+        logger.error(format_exc())
+        print(cli_errors.usage(argv[0]), file=stderr)
         exit(1)
-
-
-

a4563511e57ec3ef2ae7a2fbf9aaa75cc9767d08
jack branch <branchjohnchristopher@gmail.com>
2024-05-15 01:04:20 -0500

    added some info to the help page

--- chat_timing/src/args.rs
+++ chat_timing/src/args.rs
@@ -1,9 +1,13 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(
+    version,
+    about = "Tool used to uncover hidden messages encoded within delays over TCP servers",
+    long_about = "Tool used to uncover hidden messages encoded within delays over TCP servers. Written in Rust."
+)]
 pub struct Args {
-    #[arg(short, long, default_value = "127.0.0.1")]
+    #[arg(long, default_value = "127.0.0.1")]
     pub ip: String,
     #[arg(short, long, default_value = "1337")]
     pub port: String,

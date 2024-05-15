b4c4d34be680d8b477b3af0ac99f3e207847251b
JingusJohn <branchjohnchristopher@gmail.com>
2024-05-15 11:11:53 -0500

    merged updated chat_timing

--- chat_timing/src/main.rs
+++ chat_timing/src/main.rs
@@ -1,17 +1,18 @@
+use clap::Parser;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::time;
 
-// DELAYS (ms)
-const ONE: u128 = 100;
-const ZERO: u128 = 25;
+pub mod args;
 
-fn main() {
-    //let ip = "127.0.0.1:1337"; // my local server
-    let ip = "138.47.165.156:31337"; // timo's server
+/// Provided an IP, collect the raw timings from the server
+fn get_data(ip: String) -> Result<Vec<time::Instant>, String> {
     let mut timings = Vec::new();
     if let Ok(mut stream) = TcpStream::connect(ip) {
         let mut stdout = std::io::stdout().lock();
+        stdout
+            .write(b"Reading: ")
+            .map_err(|err| format!("Error: {}", err))?;
         let mut data = [0; 32];
         loop {
             match stream.read(&mut data) {
@@ -20,8 +21,10 @@ fn main() {
                     timings.push(time::Instant::now());
                     // print incoming unless it's EOF
                     if data[0..bytes] != [b'E', b'O', b'F'] {
-                        stdout.write(&data[0..bytes]).unwrap();
-                        stdout.flush().unwrap();
+                        stdout
+                            .write(&data[0..bytes])
+                            .map_err(|err| format!("Error: {}", err))?;
+                        stdout.flush().map_err(|err| format!("Error: {}", err))?;
                     }
                 }
                 Err(err) => {
@@ -29,52 +32,75 @@ fn main() {
                 }
             }
         }
-        stdout.write(&[b'\n']).unwrap(); // print newline
+        stdout
+            .write(&[b'\n'])
+            .map_err(|err| format!("Error: {}", err))?; // print newline
+        Ok(timings) // return timings
     } else {
-        panic!("failed to connect to server");
+        Err(String::from("failed to connect to server"))
     }
+}
 
+/// Provided timings, ONE, and ZERO split values, parse message
+fn get_msg(timings: Vec<time::Instant>, one: u128, zero: u128) -> Result<String, String> {
     // split on the average between ONE and ZERO
     // This tended to produce more consistent results in our testing
-    let split = (ONE + ZERO) / 2;
+    let split = (one + zero) / 2;
 
     let mut hidden_msg = timings
         .windows(2)
         .map(|x| x[1].duration_since(x[0]).as_millis())
         .collect::<Vec<u128>>()
         .iter()
-        .map(mapper(ONE, ZERO, split))
+        .map(mapper(one, zero, split))
         .collect::<String>()
         .as_bytes()
         .chunks(8)
-        .map(|x| std::str::from_utf8(x).unwrap())
+        .map(|x| std::str::from_utf8(x).map_err(|err| format!("Error: {}", err)))
+        .collect::<Result<Vec<&str>, _>>()?
+        .iter()
         .map(|x| format!("{:0>8}", x))
-        .map(|x| u8::from_str_radix(&x, 2).unwrap())
-        .map(|x| x as char)
+        .map(|x| u8::from_str_radix(&x, 2).map_err(|err| format!("Error: {}", err)))
+        .collect::<Result<Vec<u8>, _>>()?
+        .iter()
+        .map(|x| *x as char)
         .collect::<String>();
 
     if hidden_msg.contains("EOF") {
         hidden_msg = hidden_msg.replace("EOF", "");
     }
 
+    Ok(hidden_msg)
+}
+
+fn main() -> Result<(), String> {
+    let args = args::Args::parse();
+    if args.debug {
+        dbg!(&args);
+    }
+
+    let addr = format!("{}:{}", args.ip, args.port);
+    let timings = get_data(addr)?;
+    let hidden_msg = get_msg(timings, args.one, args.zero)?;
+
     println!("{}", hidden_msg);
+
+    Ok(())
 }
 
 /// Creates mapping closure given a one and zero value
 fn mapper(one: u128, zero: u128, split: u128) -> impl for<'a> Fn(&'a u128) -> char {
-    return move |x: &u128| {
+    move |x: &u128| {
         if one > zero {
-            if x.clone() > split {
+            if *x > split {
                 '1'
             } else {
                 '0'
             }
+        } else if *x > split {
+            '0'
         } else {
-            if x.clone() > split {
-                '0'
-            } else {
-                '1'
-            }
+            '1'
         }
-    };
+    }
 }
